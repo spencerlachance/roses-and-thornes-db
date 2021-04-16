bbb30c335d6dc7a9087363a6bf4e82f023dec9df
Brian Hughes <hughesbrian@protonmail.com>
2021-04-15 22:20:19 -0400

    updating dump file

--- dumps/dump.sql
+++ dumps/dump.sql
@@ -1,4 +1,4 @@
--- MySQL dump 10.13  Distrib 8.0.23, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.15, for macos10.14 (x86_64)
 --
 -- Host: 127.0.0.1    Database: roses_thornes
 -- ------------------------------------------------------
@@ -7,7 +7,7 @@
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
+ SET NAMES utf8 ;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -21,7 +21,7 @@
 
 DROP TABLE IF EXISTS `albums`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `albums` (
   `id` int NOT NULL AUTO_INCREMENT,
   `title` varchar(45) NOT NULL,
@@ -31,12 +31,21 @@ CREATE TABLE `albums` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `albums`
+--
+
+LOCK TABLES `albums` WRITE;
+/*!40000 ALTER TABLE `albums` DISABLE KEYS */;
+/*!40000 ALTER TABLE `albums` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `artists`
 --
 
 DROP TABLE IF EXISTS `artists`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `artists` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(45) NOT NULL,
@@ -45,12 +54,21 @@ CREATE TABLE `artists` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `artists`
+--
+
+LOCK TABLES `artists` WRITE;
+/*!40000 ALTER TABLE `artists` DISABLE KEYS */;
+/*!40000 ALTER TABLE `artists` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `classifications`
 --
 
 DROP TABLE IF EXISTS `classifications`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `classifications` (
   `classification` varchar(45) NOT NULL,
   PRIMARY KEY (`classification`),
@@ -59,12 +77,21 @@ CREATE TABLE `classifications` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `classifications`
+--
+
+LOCK TABLES `classifications` WRITE;
+/*!40000 ALTER TABLE `classifications` DISABLE KEYS */;
+/*!40000 ALTER TABLE `classifications` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `days_of_week`
 --
 
 DROP TABLE IF EXISTS `days_of_week`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `days_of_week` (
   `day_of_week` varchar(45) NOT NULL,
   PRIMARY KEY (`day_of_week`),
@@ -73,12 +100,21 @@ CREATE TABLE `days_of_week` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `days_of_week`
+--
+
+LOCK TABLES `days_of_week` WRITE;
+/*!40000 ALTER TABLE `days_of_week` DISABLE KEYS */;
+/*!40000 ALTER TABLE `days_of_week` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `episodes`
 --
 
 DROP TABLE IF EXISTS `episodes`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `episodes` (
   `season` int NOT NULL,
   `episode` int NOT NULL,
@@ -86,9 +122,18 @@ CREATE TABLE `episodes` (
   `is_finale` tinyint NOT NULL,
   `is_special` tinyint NOT NULL,
   PRIMARY KEY (`season`,`episode`),
-  CONSTRAINT `season` FOREIGN KEY (`season`) REFERENCES `seasons` (`season`)
+  CONSTRAINT `season` FOREIGN KEY (`season`) REFERENCES `seasons` (`season`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `episodes`
+--
+
+LOCK TABLES `episodes` WRITE;
+/*!40000 ALTER TABLE `episodes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `episodes` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `performances`
@@ -96,7 +141,7 @@ CREATE TABLE `episodes` (
 
 DROP TABLE IF EXISTS `performances`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `performances` (
   `id` int NOT NULL,
   `song_id` int NOT NULL,
@@ -110,12 +155,21 @@ CREATE TABLE `performances` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `performances`
+--
+
+LOCK TABLES `performances` WRITE;
+/*!40000 ALTER TABLE `performances` DISABLE KEYS */;
+/*!40000 ALTER TABLE `performances` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `seasons`
 --
 
 DROP TABLE IF EXISTS `seasons`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `seasons` (
   `season` int NOT NULL,
   `semester` varchar(45) NOT NULL,
@@ -132,12 +186,21 @@ CREATE TABLE `seasons` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `seasons`
+--
+
+LOCK TABLES `seasons` WRITE;
+/*!40000 ALTER TABLE `seasons` DISABLE KEYS */;
+/*!40000 ALTER TABLE `seasons` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `selections`
 --
 
 DROP TABLE IF EXISTS `selections`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `selections` (
   `id` int NOT NULL,
   `user_id` int NOT NULL,
@@ -153,11 +216,20 @@ CREATE TABLE `selections` (
   KEY `classification_idx` (`classification`),
   KEY `season_episode` (`season`,`episode`),
   CONSTRAINT `classification` FOREIGN KEY (`classification`) REFERENCES `classifications` (`classification`),
-  CONSTRAINT `season_episode` FOREIGN KEY (`season`, `episode`) REFERENCES `episodes` (`season`, `episode`),
+  CONSTRAINT `season_episode` FOREIGN KEY (`season`, `episode`) REFERENCES `episodes` (`season`, `episode`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `selection_song_id` FOREIGN KEY (`song_id`) REFERENCES `songs` (`id`),
   CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `selections`
+--
+
+LOCK TABLES `selections` WRITE;
+/*!40000 ALTER TABLE `selections` DISABLE KEYS */;
+/*!40000 ALTER TABLE `selections` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `semesters`
@@ -165,7 +237,7 @@ CREATE TABLE `selections` (
 
 DROP TABLE IF EXISTS `semesters`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `semesters` (
   `semester` varchar(45) NOT NULL,
   PRIMARY KEY (`semester`),
@@ -174,21 +246,39 @@ CREATE TABLE `semesters` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `semesters`
+--
+
+LOCK TABLES `semesters` WRITE;
+/*!40000 ALTER TABLE `semesters` DISABLE KEYS */;
+/*!40000 ALTER TABLE `semesters` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `songs`
 --
 
 DROP TABLE IF EXISTS `songs`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `songs` (
   `id` int NOT NULL,
   `title` varchar(45) NOT NULL,
   `album_id` int NOT NULL,
   PRIMARY KEY (`id`),
   KEY `album_id_idx` (`album_id`),
-  CONSTRAINT `album_id` FOREIGN KEY (`album_id`) REFERENCES `albums` (`id`)
+  CONSTRAINT `album_id` FOREIGN KEY (`album_id`) REFERENCES `albums` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `songs`
+--
+
+LOCK TABLES `songs` WRITE;
+/*!40000 ALTER TABLE `songs` DISABLE KEYS */;
+/*!40000 ALTER TABLE `songs` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `users`
@@ -196,7 +286,7 @@ CREATE TABLE `songs` (
 
 DROP TABLE IF EXISTS `users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `users` (
   `id` int NOT NULL AUTO_INCREMENT,
   `first_name` varchar(45) DEFAULT NULL,
@@ -206,8 +296,22 @@ CREATE TABLE `users` (
   `email` varchar(45) DEFAULT NULL,
   `date_of_birth` date DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'Brian','Hughes','bhughes','djmayhem','bhughes@rosesandthornes.com','1998-03-04'),(2,'Spencer','Lachance','slachance','djboing','slachance@rosesandthornes.com','1998-02-25'),(3,'Sean','Wallace','swallace','djdanny','swallace@rosesandthornes.com','1998-04-09');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'roses_thornes'
+--
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -218,4 +322,4 @@ CREATE TABLE `users` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-04-15 14:51:54
+-- Dump completed on 2021-04-15 22:18:12
