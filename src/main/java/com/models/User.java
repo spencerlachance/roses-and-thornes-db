c042065e3104814b2e894431174bbc8fc5d0bb8a
Brian Hughes <hughesbrian@protonmail.com>
2021-04-18 21:37:48 -0400

    committing initial version of Springboot API

---
+++ src/main/java/com/models/User.java
@@ -0,0 +1,44 @@
+package com.models;
+
+import java.util.Date;
+import javax.persistence.*;
+
+@Entity
+@Table(name="users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+    private String email;
+    private Date dateOfBirth;
+
+    public Integer getId() { return id; }
+    public void setId(Integer id) { this.id = id; }
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    public Date getDateOfBirth() { return dateOfBirth; }
+    public void setDateOfBirth(Date dateOfBirth) { this.dateOfBirth = dateOfBirth; }
+
+    public User(String username, String password, String first_name, String last_name, String email, Date dateOfBirth) {
+        this.username = username;
+        this.password = password;
+        this.firstName = first_name;
+        this.lastName = last_name;
+        this.email = email;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public User() {}
+}
---
+++ src/main/java/com/repositories/UserRepository.java
@@ -0,0 +1,18 @@
+package com.repositories;
+
+import java.util.List;
+
+import com.models.User;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+public interface UserRepository
+    extends CrudRepository<User, Integer> {
+        
+    @Query(value = "SELECT * FROM users", nativeQuery = true)
+    public List<User> findAllUsers();
+    @Query(value = "SELECT * FROM users WHERE id=:userId", nativeQuery = true)
+    public User findUserById(@Param("userId") Integer id);
+}
