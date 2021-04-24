a7253b68aa7cd1009b26048cb4c9d1f87ce9fd64
Brian Hughes <hughesbrian@protonmail.com>
2021-04-24 14:44:47 -0400

    changing list to table

--- src/main/webapp/react/episodes/episode-list.js
+++ src/main/webapp/react/episodes/episode-list.js
@@ -3,6 +3,15 @@ import episodeService from "./episode-service"
 const {useState, useEffect} = React;
 const {Link, useHistory} = window.ReactRouterDOM;
 
+function formatDate(date) {
+    let d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + (d.getDate() + 1),
+        year = d.getFullYear()
+
+    return [year, month, day].join('-')
+}
+
 const EpisodeList = () => {
     const [episodes, setEpisodes] = useState([]);
     useEffect(() => {
@@ -10,24 +19,38 @@ const EpisodeList = () => {
             .then(episodes => setEpisodes(episodes))
     }, []);
     const history = useHistory();
-
     return (
         <div>
             <h2>Episodes List</h2>
             <button className="btn btn-primary" onClick={() => history.push("/episodes/new/new")}>
                 Add Episode
             </button>
-            <ul className="list-group">
+            <table>
+                <thead className="thead">
+                <tr>
+                    <th>Season</th>
+                    <th>Episode</th>
+                    <th>Date</th>
+                    <th>isSpecial</th>
+                    <th>isFinale</th>
+                    <th>Edit</th>
+                </tr>
+                </thead>
+                <tbody>
                 {
                     episodes.map(episode =>
-                        <li key={episode.season + "-" + episode.episode}>
-                            <Link to={`/episodes/${episode.season}/${episode.episode}`}>
-                                {episode.season} {episode.episode}
-                            </Link>
-                        </li>
+                        <tr>
+                            <td>{episode.season}</td>
+                            <td>{episode.episode}</td>
+                            <td>{formatDate(episode.date)}</td>
+                            <td>{episode.isSpecial ? "true" : "false"}</td>
+                            <td>{episode.isFinale ? "true" : "false"}</td>
+                            <td><Link to={`/episodes/${episode.season}/${episode.episode}`}>editor</Link></td>
+                        </tr>
                     )
                 }
-            </ul>
+                </tbody>
+            </table>
         </div>
     )
 }
